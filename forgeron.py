154b81ebf49ff58792e224a0a02bc7c2aa3ee2a0
Your Name <you@example.com>
2021-01-30 14:59:16 +0100

    my fist commit

---
+++ forgeron.py
@@ -0,0 +1,25 @@
+#Forgeron
+
+class Forgeron:
+    
+    def welcome(self):
+        user_name = str(input("Quel est ton nom de forgeron"))
+        print("Alors bienvenue, forgeron",user_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    initmain = Forgeron()
+    initmain.welcome()
+    
